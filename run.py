2ab36a20f0ac5ec8ff5b60ee9f243cc1a20a74cd
Arthur Mårtensson <arthur0510@osloskolen.no>
2022-05-10 23:49:50 +0200

    Run script animates all scenes

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 import os
 
-with open("reciprime.py") as f:
-    lines = f.readlines()
-    scenes = [line[6:-11] for line in lines if "(m.Scene):" in line]
+filenames = [filename for filename in os.listdir()
+        if ".py" in filename and filename != "run.py"]
 
-print(os.stat("reciprime.py").st_mtime)
-for scene in scenes:
-    path = "media/videos/reciprime/480p15/" + scene + ".mp4"
-    print(os.stat(path).st_mtime)
+for filename in filenames:
+    movie_path = f"media/videos/{filename[:-3]}/480p15/{filename[:-3].capitalize()}.mp4"
+    script_mtime = os.path.getmtime(filename)
+    movie_mtime = 0
+    if os.path.exists(movie_path):
+        movie_mtime = os.path.getmtime(movie_path)
+        if movie_mtime > script_mtime:
+            print(f"Scene {filename[:-3].capitalize()} up to date")
+            continue
+    print(f"Scene {filename[:-3].capitalize()} up to date")
+    os.system(f"manim -ql {filename} {filename[:-3].capitalize()}")
+
 
