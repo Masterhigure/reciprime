33f0135b87275dac0c1c62a14eee304ae41cfecf
Arthur Mårtensson <arthur0510@osloskolen.no>
2022-05-18 15:25:23 +0200

    Reads scene names from file

--- run.py
+++ run.py
@@ -1,18 +1,25 @@
 import os
 
-filenames = [filename for filename in os.listdir()
-        if ".py" in filename and filename != "run.py"]
+scene_names = []
 
-for filename in filenames:
-    movie_path = f"media/videos/{filename[:-3]}/480p15/{filename[:-3].capitalize()}.mp4"
-    script_mtime = os.path.getmtime(filename)
+with open("scene_names.txt") as fs:
+    scene_names = [s.strip("\n") for s in fs.readlines()]
+
+for scene_name in scene_names:
+    movie_path = f"media/videos/{scene_name}/1080p60/{scene_name.capitalize()}.mp4"
+    script_mtime = os.path.getmtime(scene_name + ".py")
     movie_mtime = 0
     if os.path.exists(movie_path):
         movie_mtime = os.path.getmtime(movie_path)
         if movie_mtime > script_mtime:
-            print(f"Scene {filename[:-3].capitalize()} up to date")
+            print(f"Scene {scene_name.capitalize()} up to date")
             continue
-    print(f"Scene {filename[:-3].capitalize()} up to date")
-    os.system(f"manim -ql {filename} {filename[:-3].capitalize()}")
+    print(f"Scene {scene_name.capitalize()} not up to date")
+    os.system(f"manim -qh {scene_name}.py {scene_name.capitalize()}")
+
+with open("media/videos/tmp.txt", mode="w") as fs:
+     fs.writelines([f"file '{scene_name}/1080p60/{scene_name.capitalize()}.mp4'\n" for scene_name in scene_names])
+
+os.system("ffmpeg -f concat -i media/videos/tmp.txt -c copy output.mp4")
 
 
