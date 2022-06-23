762efe05afde8058db99088a2bd9cfdaf14c7ba2
MasterHigure <arthurmaartensson@hotmail.com>
2022-06-23 13:19:49 +0200

    Some tidying up

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ with open("scene_names.txt") as fs:
 
 for scene_name in scene_names:
     movie_path = f"media/videos/{scene_name}/1080p60/{scene_name.capitalize()}.mp4"
-    script_mtime = os.path.getmtime(scene_name + ".py")
+    script_mtime = os.path.getmtime("scripts/" + scene_name + ".py")
     movie_mtime = 0
     if os.path.exists(movie_path):
         movie_mtime = os.path.getmtime(movie_path)
@@ -15,7 +15,7 @@ for scene_name in scene_names:
             print(f"Scene {scene_name.capitalize()} up to date")
             continue
     print(f"Scene {scene_name.capitalize()} not up to date")
-    os.system(f"manim -qh {scene_name}.py {scene_name.capitalize()}")
+    os.system(f"manim -qh {'scripts/' + scene_name}.py {scene_name.capitalize()}")
 
 with open("media/videos/tmp.txt", mode="w") as fs:
      fs.writelines([f"file '{scene_name}/1080p60/{scene_name.capitalize()}.mp4'\n" for scene_name in scene_names])
