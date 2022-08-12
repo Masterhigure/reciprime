b0b7feba187b80a7043ab38e4fc24759dc82e337
Arthur <1048478+Masterhigure@users.noreply.github.com>
2022-08-12 16:44:59 +0200

    Fix damn merge issues

--- scripts/summary.py
+++ scripts/summary.py
@@ -4,7 +4,6 @@ from background import add_background
 class Summary(m.Scene):
     def construct(self):
         add_background(self)
-<<<<<<< HEAD
         title = m.Tex(r"\underline{Conclusion}", font_size=70)
         title.to_edge(m.UP)
         cdict = m.MathTex(r"2^{2k+4} < 2^{2k+4}")
@@ -13,10 +12,3 @@ class Summary(m.Scene):
         self.play(cdict.animate.scale(2))
         self.play(m.Circumscribe(cdict, time_width=2, run_time=2, buff=0.5))
         
-=======
-        title = m.Tex(r"\undeline{Conclusion)", font_size=70)
-        title.to_edge(m.UP)
-        cdict = m.MathTex(r"2^{2k+4} < 2^{2k+4}")
-
-        self.play(m.Write(title), m.Write(cdict))
->>>>>>> 3bf9e12a8c159b24be4ccd82b58f597fe7bd111c
