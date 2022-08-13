1c713df69d7d337f16c40d8056998c4bd39b674e
MasterHigure <arthurmaartensson@hotmail.com>
2022-08-13 11:58:03 +0200

    Smalls trimmed down

--- scripts/smalls.py
+++ scripts/smalls.py
@@ -4,56 +4,75 @@ from background import add_background
 class Smalls(m.Scene):
     def construct(self):
         add_background(self)
-        text1 = m.Tex(r"First we estimate $n_{s} = |N_{s}|$")
-        text2 = m.Tex(r"Take an $x\in N_{\textrm s}$")
+        title = m.Tex(r"\underline{Estimating $n_s = |N_s|$", font_size=72)
+        title.to_edge(m.UP)
+        self.play(m.Write(title))
 
-        text3a = m.MathTex(r"x = {}", "", r"p_1^{e_1}p_2^{e_2}\cdots p_k^{e_k}")
-        text3b = m.MathTex(r"x = {}", r"x_s^2\cdot {}", r"p_1^{e_1}p_2^{e_2}\cdots p_k^{e_k}")
-        text4a = m.MathTex(r"0\leq e_i", "", "")
-        text4b = m.MathTex(r"0\leq e_i", r"{}\leq 1\hphantom{al}", r"\\ 1\leq x_s\leq \sqrt K")
-        text5a = m.Tex(r"How many possibilities are there for the")
-        text5b = m.Tex(r"different parts of the right-hand side?")
-        text6a = m.Tex(r"$x_s$ has $\lfloor \sqrt K\rfloor$ possibilities")
-        text6b = m.Tex(r"Each $e_i$ has 2 possibilities")
-        text7a = m.Tex(r"Thus: ", r"$n_s \leq \sqrt K\cdot 2^k$")
-        text7b = m.Tex("", r"$n_s \leq \sqrt K\cdot 2^k$")
-
-        text1.shift(3*m.UP)
-        text2.next_to(text1, m.DOWN)
-        text3a.next_to(text2, m.DOWN)
-        text3b.next_to(text2, m.DOWN)
-        text4a.next_to(text3a, m.DOWN)
-        text4b.next_to(text3b, m.DOWN)
-        text5a.next_to(text4b, m.DOWN)
-        text5b.next_to(text5a, m.DOWN)
+        text1 = m.MathTex("r = ", r"p_1^{e_1}\cdot p_2^{e_2}\cdot p_3^{e_3}\cdots p_k^{e_k}", r"0\leq e_i")
+        # terms: text1[1][0:4] + text1[1][5:9] + text1[1][10:14] + text1[1][17:21]
+        text2 = m.MathTex("r = ", r"x^2\cdot", r"p_1^{e_1}\cdot p_2^{e_2}\cdot p_3^{e_3}\cdots p_k^{e_k}", r"0\leq e_i", r"\leq 1, \quad x^2\leq n")
+        one = m.MathTex(r"1\cdot{}")
+        text1[0:2].next_to(title, m.DOWN, buff=1)
+        text1[2].next_to(text1[0:2], m.DOWN)
+        text2[:3].move_to(text1[0:2])
+        text2[3:].next_to(text2[:3], m.DOWN)
+        one[0].move_to(m.Group(text2[1][0], text2[1][2]))
 
         self.play(m.Write(text1))
-        self.play(m.Write(text2))
-        self.play(m.AnimationGroup(m.Write(text3a), m.Write(text4a), lag_ratio = 0.8), run_time=3)
         self.wait()
-        self.play(m.ReplacementTransform(text3a, text3b),
-                  m.ReplacementTransform(text4a, text4b))
+        self.play(text1[0].animate.move_to(text2[0]),
+                text1[1].animate.move_to(text2[2]))
+        self.play(m.FadeIn(one))
         self.wait()
-        self.play(m.Write(text5a))
-        self.play(m.Write(text5b))
+
+        x_center = text2[1][0:2].get_corner(m.RIGHT + m.DOWN)
+
+        self.play(one[0][1].animate.move_to(text2[1][2]),
+                m.Transform(one[0][0], text2[1][0:2].scale(1/(1.3**3), about_point=x_center)),
+                text1[1][0:4].copy().animate.move_to(text2[1][0]).fade(1))
         self.wait()
-        self.play(m.Unwrite(text1), m.Unwrite(text2))
-        self.play(m.Group(text3b, text4b, text5b, text5a).animate.shift(2*m.UP))
+        self.play(m.AnimationGroup(text1[1][5:9].copy().animate.move_to(text2[1][0]).fade(1),
+                                   m.AnimationGroup(one[0][0].animate.scale(1.3, about_point=x_center), run_time=0.3),
+                                   lag_ratio=0.6))
         self.wait()
-        text6a.next_to(text5b, m.DOWN)
-        text6b.next_to(text6a, m.DOWN)
-        text7a.next_to(text6b, m.DOWN)
-        text7b.to_corner(m.UP + m.LEFT)
-        self.play(m.Write(text6a))
-        self.play(m.Write(text6b))
+        self.play(m.AnimationGroup(text1[1][10:14].copy().animate.move_to(text2[1][0]).fade(1),
+                                   m.AnimationGroup(one[0][0].animate.scale(1.3, about_point=x_center), run_time=0.3),
+                                   lag_ratio=0.6))
         self.wait()
-        self.play(m.Write(text7a))
-        self.play(m.Unwrite(text3b),
-                m.Unwrite(text4b),
-                m.Unwrite(text5a),
-                m.Unwrite(text5b),
-                m.Unwrite(text6a),
-                m.Unwrite(text6b))
-        self.play(m.Unwrite(text7a[0]))
-        self.play(text7a[1].animate.to_corner(m.UP + m.LEFT))
+        self.play(m.AnimationGroup(text1[1][17:21].copy().animate.move_to(text2[1][0]).fade(1),
+                                   m.AnimationGroup(one[0][0].animate.scale(1.3, about_point=x_center), run_time=0.3),
+                                   lag_ratio=0.6))
+        self.wait()
+        self.play(text1[2].animate.move_to(text2[3]), m.FadeIn(text2[4]))
+
+        text3 = m.MathTex(r"\textrm{Possibilities for }x\textrm{: }", r"\left\lfloor\sqrt{n}\right\rfloor\leq ", r"\sqrt{n}")
+        text4 = m.Tex(r"Possibilities for all the $e_i$: ", r"at most ", r"$2^k$")
+        text5 = m.Tex("Possibilities for $r$: ", r"less than ", r"$2^k\cdot \sqrt n$")
+        
+        text3[0].move_to(text4, aligned_edge=m.LEFT)
+        text5[0].move_to(text4, aligned_edge=m.LEFT)
+        text5[1:].next_to(text4[0])
+        text4[1:].move_to(text5, aligned_edge=m.RIGHT)
+        text3[1:].move_to(text5, aligned_edge=m.RIGHT)
+
+        text4.next_to(text3, m.DOWN, aligned_edge=m.LEFT)
+        text5.next_to(text4, m.DOWN, aligned_edge=m.LEFT)
+        m.Group(text3, text4, text5).next_to(text2, m.DOWN)
+
+        self.play(m.Write(text3))
+        self.wait()
+        self.play(m.Write(text4))
+        self.wait()
+        self.play(m.Write(text5))
+        self.wait()
+
+        smalls = m.MathTex(r"n_s \leq 2^k\cdot \sqrt{n}")
+        smalls.move_to(text5, aligned_edge=m.RIGHT)
+
+        self.play(m.FadeOut(text5),
+                m.FadeIn(smalls))
+        self.play(m.Indicate(smalls))
+        self.wait()
+        self.play(smalls.animate.to_corner(m.DOWN + m.LEFT),
+                m.FadeOut(m.Group(title, text1, text3, text4, one, text2[4])))
         self.wait()
