2ef34dba69d3b7f402502207929cfefb034fd775
MasterHigure <arthurmaartensson@hotmail.com>
2022-08-13 23:45:31 +0200

    Quality flexibility in run script

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import os
 
+quality = 0
+
+qfolder = ["480p15", "1080p60"]
+qcmd = ["ql", "qh"]
+
 scene_names = []
 
 with open("scene_names.txt") as fs:
     scene_names = [s.strip("\n") for s in fs.readlines()]
 
 for scene_name in scene_names:
-    movie_path = f"media/videos/{scene_name}/1080p60/{scene_name.capitalize()}.mp4"
+    movie_path = f"media/videos/{scene_name}/{qfolder[quality]}/{scene_name.capitalize()}.mp4"
     script_mtime = os.path.getmtime("scripts/" + scene_name + ".py")
     movie_mtime = 0
     if os.path.exists(movie_path):
@@ -15,10 +20,10 @@ for scene_name in scene_names:
             print(f"Scene {scene_name.capitalize()} up to date")
             continue
     print(f"Scene {scene_name.capitalize()} not up to date")
-    os.system(f"manim -qh {'scripts/' + scene_name}.py {scene_name.capitalize()}")
+    os.system(f"manim -{qcmd[quality]} {'scripts/' + scene_name}.py {scene_name.capitalize()}")
 
 with open("media/videos/tmp.txt", mode="w") as fs:
-     fs.writelines([f"file '{scene_name}/1080p60/{scene_name.capitalize()}.mp4'\n" for scene_name in scene_names])
+     fs.writelines([f"file '{scene_name}/{qfolder[quality]}/{scene_name.capitalize()}.mp4'\n" for scene_name in scene_names])
 
 os.system("ffmpeg -f concat -i media/videos/tmp.txt -c copy -y output.mp4")
 
